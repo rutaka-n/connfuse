1a5e350a3184547d1d0e1a9aca90cbd71be243d6
rutaka-n <rutaka.nashimo@yandex.ru>
2016-09-13 17:56:46 +0400

    first realization! need test and refactoring

--- lib/connfuse.rb
+++ lib/connfuse.rb
@@ -1,4 +1,80 @@
 require 'connfuse/version'
 
 module Connfuse
+  class ConnfuseError < StandardError; end
+
+  def self.included(base)
+    base.extend ClassMethods
+
+    @@timeout = 5
+    @@expected_errors = [ConnfuseError]
+    @@failure_count = 0
+    @@circuit_status = :loaded
+    @@mutex = Mutex.new
+
+#     unless self.class.expected_errors.include?(ConnfuseError)
+#       self.class.expected_errors << ConnfuseError
+#     end
+  end
+
+  module ClassMethods
+    def fuse_for(*methods)
+      methods.each do |m|
+        alias_method "#{m}_old".to_sym, m.to_sym
+        define_method(m.to_sym) do |*args|
+          pass_thru { send("#{m}_old".to_sym, *args) }
+        end
+        # private "#{m}_old".to_sym
+      end
+    end
+
+    protected :fuse_for
+  end
+
+  private
+
+  def pass_thru
+    raise ConnfuseError if broken? && too_soon?
+
+    yield
+
+  rescue => e
+    unless @@expected_errors.include? e.class
+      record_failure
+      break!
+    end
+    raise e
+  end
+
+  def broken?
+    :broken == @@circuit_status
+  end
+
+  def break!
+    @@circuit_status = :broken
+  end
+
+  def loaded?
+    :loaded == @@circuit_status
+  end
+
+  def load!
+    @@circuit_status = :loaded
+    @@failure_count = 0
+  end
+
+  def record_failure
+    @@mutex.lock
+    @@failure_count += 1
+    @@last_failed_at = Time.now
+    @@mutex.unlock
+  end
+
+  def too_soon?
+    @@last_failed_at > Time.now - @@timeout
+  end
+
+  def failure_count
+    @@failure_count
+  end
 end
--- spec/connfuse_spec.rb
+++ spec/connfuse_spec.rb
@@ -1,7 +1,20 @@
 require 'spec_helper'
+require 'pry'
 
 describe Connfuse do
   it 'has a version number' do
     expect(Connfuse::VERSION).not_to be nil
+  end
+
+  class TestClass
+    class TestError < StandardError; end
+    include Connfuse
+
+    def test(to_fail = nil)
+      raise TestError if to_fail
+      'result'
+    end
+
+    fuse_for :test
   end
 end
